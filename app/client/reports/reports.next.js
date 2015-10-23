fdcdd15cfc820a4cdbd7beb0a349b81716e75bbd
Elad Karakuli <elad.karakuli@gmail.com>
2015-10-23 12:04:15 +0300

    coloring reports by date

--- app/client/reports/reports.next.js
+++ app/client/reports/reports.next.js
@@ -9,5 +9,12 @@ angular.module('waveshout').controller('ListReportsCtrl', ['$scope', '$meteor', 
     $scope.reportThisSpot = function() {
     	$location.path('/' + $stateParams.spotId);
     }
+
+    $scope.yesterday = new Date();
+    $scope.yesterday.setDate($scope.yesterday.getDate() - 1);
+    $scope.halfday = new Date();
+    $scope.halfday.setHours($scope.halfday.getHours() - 12);
+    $scope.quarterday = new Date();
+    $scope.quarterday.setHours($scope.quarterday.getHours() - 6);
   }
 ]);
