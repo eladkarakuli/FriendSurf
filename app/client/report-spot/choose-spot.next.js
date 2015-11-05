2bab7c09d1bac7b09ff6b31df0db5f67f7bf0940
Elad Karakuli <elad.karakuli@gmail.com>
2015-11-05 12:37:36 +0200

    spot list controller, use Spots collection and template expose links

--- app/client/report-spot/choose-spot.next.js
+++ app/client/report-spot/choose-spot.next.js
@@ -2,15 +2,8 @@ angular.module('waveshout').controller('ChooseSpotCtrl', ['$scope', '$meteor',
 
   function ($scope, $meteor) {
   	$scope.$meteorSubscribe('spots');
-  	/* $scope.spots = $meteor.collection(function() {
+  	$scope.spots = $meteor.collection(function() {
   		return Spots.find();
-  	}); */
-		$scope.spots = [
-			{name: 'Bat-Yam'},
-			{name: 'Zvulun'},
-			{name: 'Rishon-Le-Zion'},
-			{name: 'Haifa-Backdoor'},
-			{name: 'Hilton'}
-		];
+  	});
   }
  ]);
