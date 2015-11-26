db8eaa77a1cc1c1e5426c13adf465b31b2d884cc
danielkazak <daniel.kazak@gmail.com>
2015-11-26 15:01:19 +0200

    Major fixes to all UI and flow

---
+++ app/client/index.next.js
@@ -0,0 +1,7 @@
+angular.module('waveshout').controller('IndexCtrl', ['$scope', '$meteor', '$stateParams',  '$location', '$timeout',
+
+  function ($scope, $meteor, $stateParams, $location, $timeout) {
+
+  	
+
+  }]);
